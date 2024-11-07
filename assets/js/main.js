15d5218d63f891b0462f0efeedb4160d3d39631a
Cribe Pencheff <cribe@self-made.se>
2024-11-07 15:10:33 +0100

    Category select, and local storage

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+import { setCategory } from './services/categoryHandler.js';
+
+const startGame = document.getElementById("start-game-section");
+
+startGame.addEventListener("click", startHandler);
+
+function startHandler(e) {
+  setCategory(e);
+}
