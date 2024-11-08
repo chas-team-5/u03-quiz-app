ab523db30c5eaf9870c12ce47ff9b55f889fdc9c
Cribe Pencheff <cribe@self-made.se>
2024-11-08 10:38:04 +0100

    JS Refactoring before merge to main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,26 @@
 import { setCategory } from './services/categoryHandler.js';
 
+const startGame = document.getElementById("start-game-section");
+
 let questions = [];
 let currentQuestion = 0;
 let score = 0;
-let selectCategory = '';
-const startGame = document.getElementById("start-game-section");
 
-startGame.addEventListener("click", startHandler);
-
-function startHandler(e) {
+function selectCategory(e) {
   setCategory(e);
   window.location.href = "quiz.html";
 }
-
 
 function saveProgress() {
     localStorage.setItem('questions', JSON.stringify(questions));
     localStorage.setItem('currentQuestion', currentQuestion);
     localStorage.setItem('score', score);
-    localStorage.setItem('selectCategory', selectCategory);
 }
 
 function loadProgress() {
     const saveQuestions = localStorage.getItem('questions');
     const savedCurrentQuestion = localStorage.getItem('currentQuestion');
     const savedScore = localStorage.getItem('score');
-    const savedSelectCategory = localStorage.getItem('selectCategory');
 }
 
-
-
-
-
+startGame.addEventListener("click", selectCategory);
