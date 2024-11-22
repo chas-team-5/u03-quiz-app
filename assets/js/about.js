a1721c83092b9fd98929d52f9bab4f70575e77c1
Cribe Pencheff <cribe@self-made.se>
2024-11-22 12:02:48 +0100

    cleanup: Remove console.log

--- assets/js/about.js
+++ assets/js/about.js
@@ -3,8 +3,6 @@ import { selectCategory } from "./services/categoryHandler.js";
 
 const buttons = document.querySelectorAll(".cta");
 
-console.log(buttons);
-
 buttons.forEach(button => button.addEventListener("click", startNewQuiz));
 
 function startNewQuiz(e) {
